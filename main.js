b4bbfa6ada08cc424aebe97f84b1f8f6cc87347b
zshakir110 <78383982+zshakir110@users.noreply.github.com>
2021-09-08 18:09:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preload() {
     img = loadImage('Bedroom.jpg');
@@ -7,12 +8,27 @@ function preload() {
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, gotResults) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
     fill("#FF0000");
-    text("Bed", 45, 75);
+    text("Bedroom", 45, 75);
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
